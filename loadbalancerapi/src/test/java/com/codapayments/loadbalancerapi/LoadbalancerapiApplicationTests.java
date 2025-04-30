712ca57e7ede065b946bf81f796ccfd45ed2d117
Tanvir Ahmad Arjel <tanvirarjel@TanvirMac.local>
2025-04-30 08:43:35 +0600

    LoadBalancerApi Test has been updated.

--- loadbalancerapi/src/test/java/com/codapayments/loadbalancerapi/LoadbalancerapiApplicationTests.java
+++ loadbalancerapi/src/test/java/com/codapayments/loadbalancerapi/LoadbalancerapiApplicationTests.java
@@ -1,13 +1,14 @@
 package com.codapayments.loadbalancerapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class LoadbalancerapiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
