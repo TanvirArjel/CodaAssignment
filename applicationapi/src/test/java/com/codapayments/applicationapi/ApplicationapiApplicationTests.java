027a237f5c121faef53f74c4329f9b1a942e80ae
Tanvir Ahmad Arjel <tanvirarjel@TanvirMac.local>
2025-04-30 08:27:39 +0600

    Application Test fixed.

--- applicationapi/src/test/java/com/codapayments/applicationapi/ApplicationapiApplicationTests.java
+++ applicationapi/src/test/java/com/codapayments/applicationapi/ApplicationapiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.codapayments.applicationapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class ApplicationapiApplicationTests {
 
 	@Test
